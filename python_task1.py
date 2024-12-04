ebe761dfa4911dc5088405db97990dbc3c302038
Muthuraj4396 <muthurajm306@gmail.com>
2024-12-04 12:06:46 +0530

    python data type declaration

---
+++ python_task1.py
@@ -0,0 +1,10 @@
+number = 10
+decimal = 10.5
+arraylist = [10, 20, 30]
+dictionarylist = {20, 30, 40}
+tuple_text = ("name1","name2")
+print (type(number))
+print (type(decimal))
+print (type(arraylist))
+print(type(dictionarylist))
+print (type(tuple_text))
